165ab9847bbdeabc741cb678b50e8812d8f71ba4
tamathecxder <yudistiraeka.pratama012@gmail.com>
2025-05-29 20:33:04 +0700

    feat(overview): basic page setup

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,12 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+    import { goto } from '$app/navigation';
+    import { onMount } from 'svelte';
+
+    onMount(() => {
+        goto('/overview');
+    });
+</script>
+
+<svelte:head>
+    <title>Home</title>
+</svelte:head>
---
+++ src/routes/overview/+page.svelte
@@ -0,0 +1,58 @@
+<script lang="ts">
+	import { LineChart } from '@carbon/charts-svelte';
+	import '@carbon/charts-svelte/styles.css';
+	// import options from './options';
+	// import data from './data';
+
+	const data: { group: string; date: string; value: number }[] = [
+		{
+			group: 'Dataset 1',
+			date: '2023-12-30',
+			value: 32100
+		},
+		{
+			group: 'Dataset 1',
+			date: '2023-12-31',
+			value: 23500
+		},
+		{
+			group: 'Dataset 1',
+			date: '2024-01-01',
+			value: 53100
+		},
+		{
+			group: 'Dataset 1',
+			date: '2024-01-02',
+			value: 42300
+		},
+		{
+			group: 'Dataset 1',
+			date: '2024-01-03',
+			value: 12300
+		}
+	];
+
+	const options: any = {
+		title: 'Rotated ticks (line)',
+		axes: {
+			bottom: {
+				scaleType: 'time',
+				mapsTo: 'date',
+				ticks: {
+					rotation: 'always'
+				}
+			},
+			left: {
+				mapsTo: 'value'
+			}
+		},
+		legend: {
+			clickable: false
+		},
+		height: '400px'
+	};
+</script>
+
+<div class="container">
+	<LineChart {data} {options} style="padding:2rem;" />
+</div>
