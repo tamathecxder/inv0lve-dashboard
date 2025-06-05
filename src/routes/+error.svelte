ddcdea57eacc3eb79aece8e1817db12022dc9d70
tamathecxder <yudistiraeka.pratama012@gmail.com>
2025-06-05 16:42:06 +0700

    chore: apply dark mode into +error

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,9 +1,21 @@
 <script lang="ts">
 	import { goto } from '$app/navigation';
 	import { page } from '$app/state';
+	import { onMount } from 'svelte';
+
+    onMount(() => {
+        if (
+            localStorage.theme === 'dark' ||
+            (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)
+        ) {
+            document.documentElement.classList.add('dark');
+        } else {
+            document.documentElement.classList.remove('dark');
+        }
+    });
 </script>
 
-<div class="flex min-h-screen flex-col items-center justify-center">
+<div class="flex min-h-screen flex-col items-center justify-center bg-white dark:bg-hytam ease-in duration-100">
 	<img src="/assets/not-found.svg" alt="404" class="w-96" />
 	<h1 class="text-9xl font-bold text-gray-700">{page?.status}</h1>
 	<p class="text-3xl font-semibold text-gray-700">{page?.error?.message}</p>
